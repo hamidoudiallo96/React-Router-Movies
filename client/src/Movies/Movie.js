8a26e7930e0d473a879eb2c195fd0d1aa01ab334
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-07-22 18:12:59 -0400

    Part 2 complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -8,7 +8,7 @@ const Movie = (props) => {
     const id = Number(props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
+    console.log(id)
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
